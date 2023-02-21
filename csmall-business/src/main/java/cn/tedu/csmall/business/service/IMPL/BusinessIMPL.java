0ff78029051ed13f25ee6629a0531c11a3199d36
zf <aaddggjj@foxmail.com>
2023-02-21 16:20:30 +0800

    seata 依赖添加

--- csmall-business/src/main/java/cn/tedu/csmall/business/service/IMPL/BusinessIMPL.java
+++ csmall-business/src/main/java/cn/tedu/csmall/business/service/IMPL/BusinessIMPL.java
@@ -2,22 +2,27 @@ package cn.tedu.csmall.business.service.IMPL;
 
 import cn.tedu.csmall.business.service.IBusiness;
 import cn.tedu.csmall.commons.pojo.order.dto.OrderAddDTO;
+import cn.tedu.csmall.order.service.service.IOrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class BusinessIMPL implements IBusiness {
-    /**
-     *
-     */
+
+    @DubboReference
+    private IOrderService dubboOrderService;
+
     @Override
     public void buy(){
         OrderAddDTO orderAddDTO = new OrderAddDTO();
-        orderAddDTO.setCount(1);
+        orderAddDTO.setCount(10);
         orderAddDTO.setUserId("uuid");
         orderAddDTO.setCommodityCode("PC100");
         orderAddDTO.setMoney(100);
         log.debug("新增加的订单信息为：",orderAddDTO);
+
+        dubboOrderService.orderAdd(orderAddDTO);
     }
 }
